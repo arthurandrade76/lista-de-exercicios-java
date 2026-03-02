d8f08c0742d76331f46092349829e30cbf1e47bf
arthurandrade76 <arthurlima2032@gmail.com>
2026-03-02 17:04:25 -0300

    Add files via upload

---
+++ Pet.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pet;
+
+/**
+ *
+ * @author pizza
+ */
+public class Pet {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
